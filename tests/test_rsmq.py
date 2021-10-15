a92458b3f17e2ffa0f60e748cdb940e748d27f49
Adam Hendel <adam.hendel@shipt.com>
2021-10-15 09:13:37 -0500

    add kafka/rsmq consumer tests

--- tests/test_rsmq.py
+++ tests/test_rsmq.py
@@ -1,5 +1,5 @@
 from engine.data_models import BundleMessage
-from engine.rsmq import BundleProducer
+from engine.rsmq import BundleConsumer, BundleProducer
 
 
 def test_rsmq_producer(
@@ -7,3 +7,10 @@ def test_rsmq_producer(
 ) -> None:
 
     assert mock_rsmq_producer.produce(queue_name="test", message=bundle_message)
+
+
+def test_rsmq_consumer(
+    mock_rsmq_consumer: BundleConsumer, bundle_message: BundleMessage
+) -> None:
+
+    assert mock_rsmq_consumer.consume(queue_name="test")

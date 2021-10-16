32575a371c2914d47e344bda96feae2b301df8ac
Adam Hendel <adam.hendel@shipt.com>
2021-10-16 10:41:14 -0500

    features created engine id

--- components/features.py
+++ components/features.py
@@ -1,5 +1,5 @@
 from typing import Dict
-
+from uuid import uuid4
 from engine.component import bundle_engine
 from engine.data_models import BundleMessage
 
@@ -10,5 +10,5 @@ OUTPUT_QUEUES = ["triage"]
 @bundle_engine(input_queue=INPUT_QUEUE, output_queues=OUTPUT_QUEUES)
 def main(message: BundleMessage) -> Dict[str, BundleMessage]:
     message.message["features"] = {"feature": "random"}
-
+    message.message["bundle_engine_id"] = str(uuid4())
     return {"triage": message}

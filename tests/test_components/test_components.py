64fb356284a6b73b5447d8a9438efdd91075b571
Adam Hendel <adam.hendel@shipt.com>
2021-10-21 10:08:58 -0500

    test componenets individually

--- tests/test_components/test_components.py
+++ tests/test_components/test_components.py
@@ -1,29 +1,37 @@
 from typing import Any, Dict
 
 from components.collector import main as collector
-from components.data_models import CollectorMessage, OutputMessage
+from components.data_models import (
+    CollectFallback,
+    CollectOptimizer,
+    CollectTriage,
+    InputMessage,
+    OutputMessage,
+    PublisherMessage,
+)
 from components.fallback import main as fallback
 from components.features import main as features
 from components.optimizer import main as optimizer
 from components.publisher import main as publisher
 from components.triage import main as triage
+from engine.data_models import ComponentMessage
 
 
-def test_features(input_message: Dict[str, Any]) -> None:
+def test_features(input_message: InputMessage) -> None:
     outputs = features.__wrapped__(input_message)
     for qname, message in outputs:
         assert qname == "triage"
-        assert isinstance(message, dict)
+        assert isinstance(message, ComponentMessage)
 
 
-def test_triage(input_message: Dict[str, Any]) -> None:
+def test_triage(input_message: InputMessage) -> None:
     triage_message = features.__wrapped__(input_message)[0][1]
     outputs = triage.__wrapped__(triage_message)
     known_out_queues = ["fallback", "collector", "optimizer"]
 
     for qname, message in outputs:
         assert qname in known_out_queues
-        assert isinstance(message, dict)
+        assert isinstance(message, ComponentMessage)
 
 
 def test_fallback_optimizer(input_message: Dict[str, Any]) -> None:
@@ -39,35 +47,21 @@ def test_fallback_optimizer(input_message: Dict[str, Any]) -> None:
     assert outputs is not None
 
 
-def test_collector(collector_message: CollectorMessage) -> None:
-    t_outputs = collector.__wrapped__(collector_message.dict())
-    assert t_outputs
-
-
-def test_publisher(collector_message: CollectorMessage) -> None:
-    msg = {"results": [collector_message.dict()]}
-    t_outputs = publisher.__wrapped__(msg)
+def test_publisher(publisher_message: PublisherMessage) -> None:
+    t_outputs = publisher.__wrapped__(publisher_message)
     for qname, m in t_outputs:
-        assert m["optimizer_type"] == "optimizer"
-        assert OutputMessage(**m)
-
-    msg["results"] = [collector_message.fallback_dict()]
-    t_outputs = publisher.__wrapped__(msg)
-    for qname, m in t_outputs:
-        assert m["optimizer_type"] == "fallback"
-        assert OutputMessage(**m)
-
-    msg["results"] = [collector_message.optimizer_dict()]
-    t_outputs = publisher.__wrapped__(msg)
-    for qname, m in t_outputs:
-        assert m["optimizer_type"] == "optimizer"
-        assert OutputMessage(**m)
+        assert m.optimizer_type == "optimizer"
+        assert isinstance(m, OutputMessage)
 
 
-def test_collector_publisher(collector_message: CollectorMessage) -> None:
-    f = collector.__wrapped__(collector_message.fallback_dict())
-    o = collector.__wrapped__(collector_message.optimizer_dict())
-    t = collector.__wrapped__(collector_message.triage_dict())
+def test_collector_publisher(
+    collector_optimizer_message: CollectOptimizer,
+    collector_fallback_message: CollectFallback,
+    collector_triage_message: CollectTriage,
+) -> None:
+    f = collector.__wrapped__(collector_fallback_message)
+    o = collector.__wrapped__(collector_optimizer_message)
+    t = collector.__wrapped__(collector_triage_message)
     for i in [f, o, t]:
         for _i in i:
             assert _i[0] == "publisher"

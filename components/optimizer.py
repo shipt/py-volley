bc71cd0186a88ebc9829941eb346973777ffcf9a
Adam Hendel <adam.hendel@shipt.com>
2021-10-22 18:49:49 -0500

    define and implement input/output data models

--- components/optimizer.py
+++ components/optimizer.py
@@ -17,7 +17,8 @@ def main(in_message: ComponentMessage) -> List[Tuple[str, ComponentMessage]]:
 
     optimizer_solution = {
         "bundles": [
-            {"group_id_1": ["order_1", "order2", "order_4"], "group_id_2": ["order_3"]},
+            {"group_id": "g1234", "orders": ["order_1", "order2", "order_4"]},
+            {"group_id": "g1235", "orders": ["order_3"]},
         ]
     }
 

84bdfc1f1a12fbb72647e63242f1f6ab16262f5e
Adam Hendel <adam.hendel@shipt.com>
2021-10-28 09:17:25 -0500

    fix lints

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -1,10 +1,7 @@
-import logging
 import os
 import time
 
 import confluent_kafka.admin
-
-logger = logging.getLogger(__name__)
 
 # time for Kafka broker to init
 time.sleep(5)
@@ -16,6 +13,3 @@ conf = {"bootstrap.servers": brokers}
 admin = confluent_kafka.admin.AdminClient(conf)
 topics = [confluent_kafka.admin.NewTopic(x, 1, 1) for x in [input_topic, output_topic]]
 admin.create_topics(topics)
-
-logger.info(f"CREATED_TOPICS: {input_topic} - {output_topic}")
-logger.info(f"BROKERS: {brokers} - {output_topic}")

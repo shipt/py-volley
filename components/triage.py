8c7bbe26d78476bd9ae482c5e9cf87908604fd7a
Adam Hendel <adam.hendel@shipt.com>
2021-10-29 19:42:05 -0500

    use data model

--- components/triage.py
+++ components/triage.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
-from typing import List, Tuple
+from typing import Any, Dict, List, Tuple
+
+import pandas as pd
 
 from components.data_models import CollectTriage, TriageMessage
 from engine.data_models import ComponentMessage
@@ -14,26 +16,31 @@ OPT_TIMEOUT_SECONDS = 60
 @bundle_engine(input_queue=INPUT_QUEUE, output_queues=OUTPUT_QUEUES)
 def main(in_message: TriageMessage) -> List[Tuple[str, ComponentMessage]]:
     message = in_message.copy()
-    message.message["triage"] = {"triage": ["a", "b"]}
-
+    enriched_orders: List[Dict[str, Any]] = in_message.enriched_orders
     # # GROUP ORDERS BY TIME WINDOW
-    # order_lst = message.message.get("order_list")
-    # orders = pd.concat(orders_lst)
-    # orders['TW_HR'] = pd.to_datetime(orders['delivery_start_time']).dt.hour
-    # orders = orders.sort_values('TW_HR')
-    # grouped_orders = []
-    # for hours in orders['TW_HR']:
-    #     orders = orders[orders['TW_HR'] == hours]
-    #     grouped_orders.append(orders)
+    orders_lst = enriched_orders
+    orders = pd.concat(orders_lst)
+    orders['TW_HR'] = pd.to_datetime(orders['delivery_start_time']).dt.hour
+    orders = orders.sort_values('TW_HR')
+    grouped_orders = []
+    for hours in orders['TW_HR']:
+        orders = orders[orders['TW_HR'] == hours]
+        grouped_orders.append(orders)
 
     t = CollectTriage(
-        engine_event_id=message["engine_event_id"],
-        bundle_request_id=message["bundle_request_id"],
+        engine_event_id=message.engine_event_id,
+        bundle_request_id=message.bundle_request_id,
         timeout=str(datetime.now() + timedelta(seconds=OPT_TIMEOUT_SECONDS)),
     )
 
+    opt_fall_msg = ComponentMessage(
+            engine_event_id=message.engine_event_id,
+            bundle_request_id=message.bundle_request_id,
+            message=grouped_orders
+    )
+
     return [
-        ("optimizer", in_message),
-        ("fallback", in_message),
+        ("optimizer", opt_fall_msg),
+        ("fallback", opt_fall_msg),
         ("collector", t),
     ]

a45c2970084af650a0422fb93b394906315a1dab
Adam Hendel <adam.hendel@shipt.com>
2021-10-31 13:52:57 -0500

    single name for metrics, added a type

--- monitoring/monitoring.py
+++ monitoring/monitoring.py
@@ -12,8 +12,7 @@ eng = get_eng()
 
 all_queues = [x["value"] for x in load_config()["queues"] if x["type"] == "rsmq"]
 
-g_redis = Gauge("rsmq_queue_length", "Length of queues", ["queue_name"])
-g_postgres = Gauge("psql_queue_length", "Length of queues", ["queue_name"])
+g = Gauge("bundle_engine_queue_length", "Length of queues", ["queue_name", "queue_type"])
 
 
 def get_publisher_queue_size() -> int:
@@ -31,10 +30,10 @@ def generate_metrics() -> None:
     for q in all_queues:
         _q = f"rsmq:{q}"
         size = r.zcard(_q)
-        g_redis.labels(q).set(size)
+        g.labels(queue_name=q, queue_type="rsmq").set(size)
 
     publisher_size = get_publisher_queue_size()
-    g_postgres.labels("publisher").set(publisher_size)
+    g.labels(queue_name="publisher", queue_type="postgres").set(publisher_size)
 
 
 wsgi_app = make_wsgi_app()

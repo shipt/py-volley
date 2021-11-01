dc3a9aae7fcf56b2ae538e7d69f858b9e14d7e0a
Adam Hendel <adam.hendel@shipt.com>
2021-11-01 16:50:54 -0500

    add pandas

--- tests/test_components/test_optimizer.py
+++ tests/test_components/test_optimizer.py
@@ -1,10 +1,30 @@
+from typing import Any, Dict, List
+from unittest.mock import MagicMock, patch
+
 import pytest
 
+from components.data_models import CollectOptimizer, OptimizerMessage
 from components.optimizer import main as optimizer
 
-# TEST NULL MESSAGE
-def test_optimizer_null_message(null_input_message: InputMessage) -> None:
-    with pytest.raises(Exception):
-        outputs = optimizer.__wrapped__(null_input_message)
 
-# TEST INVALID INPUT
+def mock_optimizer(orders: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """dummy optimizer - creates one giant bundle"""
+    return {
+        "bundle_request_id": "string",
+        "bundles": [{"group_id": "group-id-1", "orders": [x["order_id"] for x in orders]}],
+    }
+
+
+@patch("components.optimizer.requests.post")
+def test_optimizer(mock_post: MagicMock) -> None:
+    mock_post.return_value.status_code = 200
+
+    msg = OptimizerMessage(**OptimizerMessage.schema()["examples"][0])
+    expected_response = mock_optimizer(msg.grouped_orders[0])
+
+    mock_post.return_value.json = lambda: expected_response
+    outputs = optimizer.__wrapped__(msg)
+
+    for qname, output in outputs:
+        assert isinstance(output, CollectOptimizer)
+        assert output.event_type == "optimizer"

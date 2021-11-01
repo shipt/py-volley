b4cacf83e74799f2e5b07832baafc48ed4aa53e1
Adam Hendel <adam.hendel@shipt.com>
2021-11-01 17:25:10 -0500

    big merge conflict

--- components/triage.py
+++ components/triage.py
@@ -3,39 +3,39 @@ from typing import Any, Dict, List, Tuple
 
 import pandas as pd
 
-from components.data_models import CollectTriage, TriageMessage
+from components.data_models import CollectTriage, OptimizerMessage, TriageMessage
 from engine.data_models import ComponentMessage
 from engine.engine import bundle_engine
 
 INPUT_QUEUE = "triage"
 OUTPUT_QUEUES = ["optimizer", "fallback", "collector"]  # , "shadow"]
 
-OPT_TIMEOUT_SECONDS = 60
+OPT_TIMEOUT_SECONDS = 120
 
 
 @bundle_engine(input_queue=INPUT_QUEUE, output_queues=OUTPUT_QUEUES)
 def main(in_message: TriageMessage) -> List[Tuple[str, ComponentMessage]]:
-    message = in_message.copy()
     enriched_orders: List[Dict[str, Any]] = in_message.enriched_orders
+
     # # GROUP ORDERS BY TIME WINDOW
-    orders_lst = enriched_orders
-    orders = pd.DataFrame(orders_lst)
-    orders['TW_HR'] = pd.to_datetime(orders['delivery_start_time']).dt.hour
-    orders = orders.sort_values('TW_HR')
+    orders = pd.DataFrame(enriched_orders)
+    orders["TW_HR"] = pd.to_datetime(orders["delivery_start_time"]).dt.hour
+    orders = orders.sort_values("TW_HR")
 
     group_df = orders.groupby("TW_HR")
-    grouped_orders = [group.to_dict(orient="records") for _,group in group_df]
+    grouped_orders = [group.to_dict(orient="records") for _, group in group_df]
 
     t = CollectTriage(
-        engine_event_id=message.engine_event_id,
-        bundle_request_id=message.bundle_request_id,
+        engine_event_id=in_message.engine_event_id,
+        bundle_request_id=in_message.bundle_request_id,
         timeout=str(datetime.now() + timedelta(seconds=OPT_TIMEOUT_SECONDS)),
     )
 
-    opt_fall_msg = ComponentMessage(
-            engine_event_id=message.engine_event_id,
-            bundle_request_id=message.bundle_request_id,
-            message=grouped_orders
+    opt_fall_msg = OptimizerMessage(
+        engine_event_id=in_message.engine_event_id,
+        bundle_request_id=in_message.bundle_request_id,
+        grouped_orders=grouped_orders,
+        error_orders=in_message.error_orders,
     )
 
     return [

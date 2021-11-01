563465ddd0a29cb528db1738fdf635227111ec26
Adam Hendel <adam.hendel@shipt.com>
2021-11-01 08:39:14 -0500

    change FP errors to error level

--- components/features.py
+++ components/features.py
@@ -34,7 +34,7 @@ def main(in_message: InputMessage) -> List[Tuple[str, ComponentMessage]]:
                 stops = fp_data["route"]["stops"]
                 for i, stop in enumerate(fp_data["route"]["stops"]):
                     if i > 1:
-                        logger.warning(f"More than one pick one drop in order: {stops}")
+                        logger.error(f"More than one pick one drop in order: {stops}")
                     stop_type = stop["type"]  # pickup or delivery
 
                     geo_data[stop_type] = {"latitude": stop["latitude"], "longitude": stop["longitude"]}
@@ -61,7 +61,7 @@ def main(in_message: InputMessage) -> List[Tuple[str, ComponentMessage]]:
                     }
                 )
         else:
-            logger.warning(f"Flight-plan returned status code: {resp.status_code} for order: {order}")
+            logger.error(f"Flight-plan returned status code: {resp.status_code} for order: {order}")
             raw_orders.append(
                 {
                     "order_id": order,

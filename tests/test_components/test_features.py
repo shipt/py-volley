9426d276420c9becc344089f9b53f4efa0d2413d
Adam Hendel <adam.hendel@shipt.com>
2021-11-02 18:33:34 -0500

    unit test for metro calls

---
+++ tests/test_components/test_features.py
@@ -0,0 +1,57 @@
+from unittest.mock import MagicMock, patch
+
+from components.data_models import InputMessage, TriageMessage
+from components.features import get_metro_attr, get_shop_time
+from components.features import main as features
+
+
+def test_fp_default() -> None:
+    fp_result = get_shop_time("16578146")
+    assert fp_result["shop_time_minutes"] > 0
+
+
+def test_get_metro_attr_default() -> None:
+    metro_result = get_metro_attr("2110")
+    assert metro_result["store_location_latitude"] == 42.9967809
+    assert metro_result["store_location_longitude"] == -85.59336449999999
+
+
+def test_features(input_message: InputMessage, fp_service_response: Dict[str, Any]) -> None:
+    with patch("components.features.requests.get") as fp_success:
+        fp_success.return_value.status_code = 200
+        fp_success.return_value.json = lambda: fp_service_response
+        outputs = features.__wrapped__(input_message)
+
+        for qname, message in outputs:
+            assert qname == "triage"
+            assert isinstance(message, TriageMessage)
+            assert isinstance(message.enriched_orders, list)
+
+            for order in message.enriched_orders:
+                assert order["order_id"] in input_message.orders
+                # spot check some attributes exist
+                assert isinstance(order["delv_longitude"], float)
+                assert isinstance(order["delv_latitude"], float)
+                assert order["item_qty"] > 0
+
+
+@patch("components.features.requests.get")
+def test_bunk_order_id(mock_get: MagicMock) -> None:
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json = lambda: {"order_id": 1}
+    f = InputMessage(bundle_request_id="a1234", orders=["1", "2", "3"])
+    outputs = features.__wrapped__(f)  # NOQA: F841
+    for qname, message in outputs:
+        assert len(message.error_orders) == 3
+        assert len(message.enriched_orders) == 0
+
+
+@patch("components.features.requests.get")
+def test_bunk_fp_response(mock_get: MagicMock, input_message: InputMessage) -> None:
+    mock_get.return_value.status_code = 500
+    mock_get.return_value.json = lambda: {"order_id": 1}
+    outputs = features.__wrapped__(input_message)  # NOQA: F841
+    for qname, message in outputs:
+        # forcing bad response from FP - so all should be "error orders"
+        assert len(message.error_orders) == len(input_message.orders)
+        assert len(message.enriched_orders) == 0

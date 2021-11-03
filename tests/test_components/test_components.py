800bbfca02379840576059675b51c0c65df565b9
Adam Hendel <adam.hendel@shipt.com>
2021-11-02 20:13:10 -0500

    handle serialization of timestap to optimizer

--- tests/test_components/test_components.py
+++ tests/test_components/test_components.py
@@ -17,12 +17,11 @@ from components.optimizer import main as optimizer
 from components.publisher import main as publisher
 from components.triage import main as triage
 from engine.data_models import ComponentMessage
+from tests.test_components.test_features import mocked_requests_get
 
 
-@patch("components.features.requests.get")
+@patch("components.features.requests.get", side_effect=mocked_requests_get)
 def test_triage(mock_get: MagicMock, input_message: InputMessage, fp_service_response: Dict[str, Any]) -> None:
-    mock_get.return_value.status_code = 200
-    mock_get.return_value.json = lambda: fp_service_response
 
     triage_message = features.__wrapped__(input_message)[0][1]
     outputs = triage.__wrapped__(triage_message)
@@ -33,13 +32,10 @@ def test_triage(mock_get: MagicMock, input_message: InputMessage, fp_service_res
         assert isinstance(message, ComponentMessage)
 
 
-@patch("components.features.requests.get")
+@patch("components.features.requests.get", side_effect=mocked_requests_get)
 def test_fallback_optimizer(
     mock_get: MagicMock, input_message: Dict[str, Any], fp_service_response: Dict[str, Any]
 ) -> None:
-    mock_get.return_value.status_code = 200
-    mock_get.return_value.json = lambda: fp_service_response
-
     triage_message = features.__wrapped__(input_message)[0][1]
     t_outputs = triage.__wrapped__(triage_message)
     outputs = None

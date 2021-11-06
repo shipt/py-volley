b88ae232694417f733876ef383e917d47e5b20b4
Adam Hendel <adam.hendel@shipt.com>
2021-11-05 21:53:05 -0500

    add kafka test

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,9 +1,10 @@
-from typing import List, Optional, Tuple
+from typing import List, Tuple
 from unittest.mock import MagicMock, patch
 
 import pytest
 
 from engine.engine import bundle_engine, get_consumer, get_producer
+from tests.test_kafka import KafkaMessage
 
 
 @patch("engine.kafka.KafkaConsumer")
@@ -30,25 +31,6 @@ def test_get_producer(mock_redis, mock_kafka) -> None:  # type: ignore
         producer = get_producer("non-existant-queue", qname)
 
 
-class KafkaMessage:
-
-    _error_msg = "MOCK ERORR"
-
-    def __init__(self, error: bool = False) -> None:
-        self.is_error = error
-
-    def offset(self) -> int:
-        return 123
-
-    def value(self) -> bytes:
-        return b'{"random": "message"}'
-
-    def error(self) -> Optional[str]:
-        if self.is_error:
-            return self._error_msg
-        return None
-
-
 @patch("engine.engine.RUN_ONCE", True)
 @patch("engine.kafka.KafkaProducer")
 @patch("engine.kafka.KafkaConsumer")
@@ -56,6 +38,19 @@ def test_engine(mock_consumer: MagicMock, mock_producer: MagicMock) -> None:
     mock_consumer.return_value.poll = lambda x: KafkaMessage()
 
     @bundle_engine(input_queue="input-queue", output_queues=["output-queue"])
+    def func() -> List[Tuple[None, None]]:
+        return [(None, None)]
+
+    func()
+
+
+@patch("engine.engine.RUN_ONCE", True)
+@patch("engine.kafka.KafkaProducer")
+@patch("engine.kafka.KafkaConsumer")
+def test_engine_not_exist(mock_consumer: MagicMock, mock_producer: MagicMock) -> None:
+    mock_consumer.return_value.poll = lambda x: KafkaMessage()
+
+    @bundle_engine(input_queue="input-queue", output_queues=["NOT-EXIST"])
     def func() -> List[Tuple[None, None]]:
         return [(None, None)]
 

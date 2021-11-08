60fd32bdcba017b65f19049bda24480e17e99420
shipt-production-apps <40867493+shipt-production-apps@users.noreply.github.com>
2021-11-08 10:04:35 -0600

    Infrastructure Changes (#52)
    * infrastructure_changes
    
    * infrastructure_changes
    
    Co-authored-by: Adam Hendel <ChuckHend@users.noreply.github.com>

--- terraform/app-gcp-ml-bundle-engine-stg-central1/dba.tf
+++ terraform/app-gcp-ml-bundle-engine-stg-central1/dba.tf
@@ -1,6 +1,6 @@
 module "ml_bundle_engine_redis_queue_memory_store" {
   source         = "app.terraform.io/shipt/memorystore/google"
-  version        = "1.0.3"
+  version        = "1.0.4"
   memory_size_gb = "1"
   name           = "uc1-ml-bundle-engine-redis-queue"
   project        = "shipt-ds-stg-redis"
@@ -16,6 +16,7 @@ module "ml_bundle_engine_redis_queue_memory_store" {
   authorized_network = "projects/shipt-host/global/networks/host"
   redis_configs = {
     activedefrag     = "no"
+    maxmemory-gb     = "1"
     maxmemory-policy = "volatile-lru"
   }
   connect_mode  = "PRIVATE_SERVICE_ACCESS"
--- terraform/app-gcp-ml-bundle-engine-stg-central1/ml-bundle-engine.tf
+++ terraform/app-gcp-ml-bundle-engine-stg-central1/ml-bundle-engine.tf
@@ -14,6 +14,22 @@ module "ml_bundle_engine_bundle_engine_input_bus_ds_marketplace_v1_bundle_engine
   }
   partitions = 3
 }
+module "ml_bundle_engine_bundle_engine_dlq_bus_ds_marketplace_v1_bundle_engine_dlq_topic_module" {
+  source         = "app.terraform.io/shipt/topic/kafka"
+  version        = "0.4.1"
+  application    = "ml-bundle-engine"
+  group          = "ds"
+  environment    = "staging"
+  config_var     = "INPUT_TOPIC"
+  config_regions = ["us-central1"]
+  name           = "stg.bus.ds-marketplace.v1.bundle_engine_dlq"
+  config = {
+    "cleanup.policy"  = "delete"
+    "retention.bytes" = "-1"
+    "retention.ms"    = "259200000"
+  }
+  partitions = 3
+}
 module "ml_bundle_engine_bundle_engine_output_bus_ds_marketplace_v1_bundle_engine_output_topic_module" {
   source         = "app.terraform.io/shipt/topic/kafka"
   version        = "0.4.1"

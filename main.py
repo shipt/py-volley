62e48ebde4890f1c41bae8a4c5b3460c1f690b21
Adam Hendel <adam.hendel@shipt.com>
2021-11-10 15:56:37 -0600

    explicit exception

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ rollbar.init(os.getenv("ROLLBAR_TOKEN"), os.getenv("APP_ENV", "localhost"))
 def run() -> None:
     # TODO: can we get rid of this file entirely? just call each component.py directly?
     component = sys.argv[1]
-
     if component == "dummy_events":
         from components.dummy_events import main
     elif component == "features":

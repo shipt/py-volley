50b7fcceb62f7c2f7c37c0a7c9ab6f673d171976
Adam Hendel <adam.hendel@shipt.com>
2021-11-15 19:11:47 -0600

    add classes as attr to objects

--- volley/queues.py
+++ volley/queues.py
@@ -1,10 +1,11 @@
+import abc
 from enum import Enum
 from typing import Dict, Optional, Union
 
 from jinja2 import Template
 from pydantic import BaseModel
 
-from volley.config import ENV, load_config
+from volley.config import ENV, get_application_config, import_module_from_string
 from volley.connectors.base import Consumer, Producer
 
 
@@ -18,6 +19,9 @@ class Queue(BaseModel):
     model_schema: str
     type: QueueType
 
+    consumer_class: abc.ABCMeta
+    producer_class: abc.ABCMeta
+
     # queue connection
     # TODO: figure out a good place for this to live
     # it should probably never take on a None value
@@ -29,7 +33,7 @@ class Queues(BaseModel):
 
 
 def available_queues() -> Queues:
-    cfg = load_config()
+    cfg = get_application_config()
 
     kafka_env_map = {
         "production": "prd",
@@ -47,7 +51,14 @@ def available_queues() -> Queues:
             _value = _t.render(env=kafka_env)
         else:
             _value = q["value"]
-        meta = Queue(value=_value, type=q["type"], model_schema=q["schema"])
+
+        meta = Queue(
+            value=_value,
+            type=q["type"],
+            model_schema=q["schema"],
+            consumer_class=import_module_from_string(q["consumer"]),
+            producer_class=import_module_from_string(q["producer"]),
+        )
         queues[q["name"]] = meta
 
     return Queues(queues=queues)

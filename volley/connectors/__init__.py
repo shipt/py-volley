e6c8fa4beb094864949c42bc51503eaebb005f18
Adam Hendel <adam.hendel@shipt.com>
2021-11-15 23:13:51 -0600

    cleanup

--- volley/connectors/__init__.py
+++ volley/connectors/__init__.py
@@ -1,5 +1,4 @@
 from volley.connectors.kafka import KafkaConsumer, KafkaProducer
-from volley.connectors.postgres import PGConsumer, PGProducer
 from volley.connectors.rsmq import RSMQConsumer, RSMQProducer
 
-__all__ = ["KafkaConsumer", "KafkaProducer", "PGConsumer", "PGProducer", "RSMQConsumer", "RSMQProducer"]
+__all__ = ["KafkaConsumer", "KafkaProducer", "RSMQConsumer", "RSMQProducer"]

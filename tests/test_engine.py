419bce7c68671e06c656cafc34056a57539b8bdb
Adam Hendel <adam.hendel@shipt.com>
2021-11-17 09:48:51 -0600

    revert dup test

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -57,28 +57,3 @@ def test_rsmq_component(mock_rsmq: MagicMock) -> None:
 
     # must not raise any exceptions
     hello_world()
-
-
-@patch("volley.engine.RUN_ONCE", True)
-@patch("volley.engine.METRICS_ENABLED", False)
-@patch("volley.connectors.rsmq.RedisSMQ")
-def test_schema_validation_error(mock_rsmq) -> None:
-    m = {"uuid": str(uuid4)}
-    rsmq_msg = {
-        "id": "rsmq_id",
-        "message": json.dumps(m),
-    }
-    mock_rsmq.return_value.receiveMessage.return_value.exceptions.return_value.execute = lambda: rsmq_msg
-
-    mock_rsmq.return_value.sendMessage.return_value.execute = lambda: True
-    eng = Engine(input_queue="comp_1", output_queues=["comp_1"])
-
-    @eng.stream_app
-    def hello_world(msg: ComponentMessage) -> List[Tuple[str, ComponentMessage]]:
-        msg_dict = msg.dict()
-        unique_val = msg_dict["uuid"]
-        out = ComponentMessage(hello="world", unique_val=unique_val)
-        return [("output-queue", out)]
-
-    # must not raise any exceptions
-    hello_world()

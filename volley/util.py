5bebf931c5444c74c757401bcedb63846b91a4db
Adam Hendel <adam.hendel@shipt.com>
2021-11-17 18:50:26 -0600

    add graceful killer

---
+++ volley/util.py
@@ -0,0 +1,13 @@
+import signal
+from typing import Any
+
+
+class GracefulKiller:
+    kill_now = False
+
+    def __init__(self) -> None:
+        signal.signal(signal.SIGINT, self.exit_gracefully)
+        signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+    def exit_gracefully(self, signum: int, frame: Any) -> None:
+        self.kill_now = True

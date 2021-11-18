5f5ad6266d8ebc609d4ca3a25f561df73c6b89a0
Adam Hendel <adam.hendel@shipt.com>
2021-11-18 17:58:29 -0600

    update example project

--- example/external_data_producer.py
+++ example/external_data_producer.py
@@ -1,18 +1,19 @@
 # example producer
 # simulates some "external service" publishing data to a kafka topic that a Volley application consumes from
 import time
+from typing import Dict
 from uuid import uuid4
 
 from pyshipt_streams import KafkaProducer
 
 from example.data_models import InputMessage
 from volley.logging import logger
-from volley.queues import available_queues
+from volley.queues import Queue, available_queues
 
 
 def main() -> None:
-    queues = available_queues()
-    input_topic = queues.queues["input-queue"].value
+    queues: Dict[str, Queue] = available_queues()
+    input_topic = queues["input-queue"].value
     logger.info(f"{input_topic=}")
     p = KafkaProducer()
     i = 0
@@ -20,6 +21,7 @@ def main() -> None:
         data = InputMessage.schema()["examples"][0]
         uuid = str(uuid4())[:8]
         data["request_id"] = f"{uuid}-{i}"
+        del data["request_id"]
         p.publish(input_topic, data, serialize=True)
         logger.info(f"{data=}")
         time.sleep(10)

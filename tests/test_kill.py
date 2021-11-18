5f2325ec28779e868f89c73cf3f8bf3f95c4963a
Adam Hendel <adam.hendel@shipt.com>
2021-11-17 18:50:12 -0600

    add test for killer

---
+++ tests/test_kill.py
@@ -0,0 +1,17 @@
+import threading
+from unittest.mock import MagicMock, patch
+
+from example.input_component import eng, main
+
+
+@patch("volley.engine.METRICS_ENABLED", False)
+@patch("volley.connectors.rsmq.RSMQProducer", MagicMock())
+@patch("volley.connectors.kafka.KConsumer", MagicMock())
+def test_graceful_kill() -> None:
+    t = threading.Thread(target=main, daemon=True)
+    t.start()
+
+    eng.killer.exit_gracefully(100, None)
+    assert eng.killer.kill_now is True
+    t.join(timeout=10.0)
+    assert not t.is_alive()

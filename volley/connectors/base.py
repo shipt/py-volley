450fa62e9c26ecaef5414ce5506f0780d0d72e10
Adam Hendel <adam.hendel@shipt.com>
2021-11-20 14:24:30 -0600

    dead code in base class

--- volley/connectors/base.py
+++ volley/connectors/base.py
@@ -39,8 +39,6 @@ class Producer(ABC):
     @abstractmethod
     def produce(self, queue_name: str, message: QueueMessage) -> bool:
         """publishes a message to any queue"""
-        logger.info(f"producing to: {queue_name=}")
-        return True
 
     def shutdown(self) -> None:
         """perform some action when shutting down the application"""

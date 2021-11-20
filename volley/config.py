b6cc4df007f111e93bd095b4515ada7e999d41f0
Adam Hendel <adam.hendel@shipt.com>
2021-11-20 13:20:54 -0600

    explicit DLQ

--- volley/config.py
+++ volley/config.py
@@ -1,7 +1,7 @@
 import importlib
 import os
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict, Union
 
 import yaml  # type: ignore
 from yaml import Loader
@@ -15,61 +15,22 @@ METRICS_ENABLED = True
 METRICS_PORT = 3000
 
 
-def load_yaml(file_path: Path) -> Dict[str, Any]:
-    """loads a yaml to dict from Path object"""
-    with file_path.open() as f:
-        cfg: Dict[str, Any] = yaml.load(f, Loader=Loader)
-    return cfg
+def load_yaml(file_path: Union[str, Path]) -> Dict[str, Any]:
+    """loads a yaml to dict from Path object
 
-
-def load_client_config(yaml_path: str) -> Dict[str, List[Dict[str, str]]]:
-    """attemps to load the client provided config yaml
-
-    raises FileNotFoundError
+    Raises FileNotFoundError
     """
-    yaml_cfg = Path(yaml_path)
-    cfg: Dict[str, List[Dict[str, str]]] = {}
+    if isinstance(file_path, str):
+        path = Path(file_path)
+    else:
+        path = file_path
     try:
-        cfg = load_yaml(yaml_cfg)
+        with path.open() as f:
+            cfg: Dict[str, Any] = yaml.load(f, Loader=Loader)
     except FileNotFoundError:
-        logger.error(f"file {yaml_cfg} not found - falling back to default for testing")
+        logger.error(f"file {file_path} not found")
         raise
     return cfg
-
-
-def load_queue_configs(yaml_path: str) -> Dict[str, Dict[str, str]]:
-    """loads client configurations for:
-        - queues
-        - connectors
-        - data classes/schemas
-
-    falls back to global configurations when client does not provide them
-    """
-    client_cfg: Dict[str, List[Dict[str, str]]] = load_client_config(yaml_path=yaml_path)
-    return {k["name"]: k for k in client_cfg["queues"]}
-
-
-def apply_defaults(config: Dict[str, List[Dict[str, str]]]) -> Dict[str, Dict[str, str]]:
-    global_configs = load_yaml(GLOBALS)
-    # handle default fallbacks
-    global_connectors = global_configs["connectors"]
-    default_queue_schema = global_configs["schemas"]["default"]
-
-    queue_dict: Dict[str, Dict[str, str]] = {}
-    for q in config["queues"]:
-        # for each defined queue, validate there is a consumer & producer defined
-        # or fallback to the global default
-        q_type = q["type"]
-        for conn in ["consumer", "producer"]:
-            if conn not in q:
-                # if there isn't a connector (produce/consume) defined,
-                #   assign it from global defalt
-                q[conn] = global_connectors[q_type][conn]
-        # handle data schema
-        if "schema" not in q:
-            q["schema"] = default_queue_schema
-        queue_dict[q["name"]] = q
-    return queue_dict
 
 
 def import_module_from_string(module_str: str) -> Any:

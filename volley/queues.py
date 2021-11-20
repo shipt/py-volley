c66208eea402b6e659d6f37d289caa82ae25f790
Adam Hendel <adam.hendel@shipt.com>
2021-11-19 20:24:47 -0600

    pass config to initializer

--- volley/queues.py
+++ volley/queues.py
@@ -4,7 +4,12 @@ from typing import Any, Dict, List, Union
 
 from jinja2 import Template
 
-from volley.config import APP_ENV, import_module_from_string, load_queue_configs
+from volley.config import (
+    APP_ENV,
+    apply_defaults,
+    import_module_from_string,
+    load_queue_configs,
+)
 from volley.connectors.base import Consumer, Producer
 from volley.data_models import ComponentMessage
 from volley.logging import logger
@@ -28,11 +33,11 @@ class Queue:
     # system name for the queue. for example, some really long kafka topic name
     value: str
 
-    model_schema: str
+    schema: str
     type: str
 
-    consumer_class: str
-    producer_class: str
+    consumer: str
+    producer: str
 
     # initialized queue connection
     # these get initialized by calling connect()
@@ -45,19 +50,19 @@ class Queue:
 
     def connect(self, con_type: ConnectionType) -> None:
         if con_type == ConnectionType.CONSUMER:
-            _class = import_module_from_string(self.consumer_class)
+            _class = import_module_from_string(self.consumer)
             self.consumer_con = _class(queue_name=self.value)
         elif con_type == ConnectionType.PRODUCER:
-            _class = import_module_from_string(self.producer_class)
+            _class = import_module_from_string(self.producer)
             self.producer_con = _class(queue_name=self.value)
         else:
             logger.error(f"{con_type=} is not valid")
 
     def init_schema(self) -> None:
-        if self.model_schema in ["dict"]:
+        if self.schema in ["dict"]:
             self.model_class = dict  # type: ignore
         else:
-            self.model_class = import_module_from_string(self.model_schema)
+            self.model_class = import_module_from_string(self.schema)
 
 
 def queues_from_yaml(queues: List[str]) -> Dict[str, Queue]:
@@ -65,24 +70,9 @@ def queues_from_yaml(queues: List[str]) -> Dict[str, Queue]:
 
     Returns a map of {queue_name: Queue}
     """
-    cfg = load_queue_configs()
+    cfg: Dict[str, Dict[str, str]] = load_queue_configs()
 
-    input_output_queues: Dict[str, Queue] = {}
-
-    for q in queues:
-        q_config = interpolate_kafka_topics(cfg[q])
-        try:
-            input_output_queues[q] = Queue(
-                name=q,
-                value=q_config["value"],
-                model_schema=q_config["schema"],
-                type=q_config["type"],
-                consumer_class=q_config["consumer"],
-                producer_class=q_config["producer"],
-            )
-        except KeyError:
-            logger.warning(f"Queue '{q}' not found in configuraiton")
-    return input_output_queues
+    return queues_from_dict(cfg)
 
 
 def interpolate_kafka_topics(cfg: Dict[str, str]) -> Dict[str, str]:
@@ -115,10 +105,39 @@ def available_queues() -> Dict[str, Queue]:
             name=q_name,
             value=queue_dict["value"],
             type=queue_dict["type"],
-            model_schema=queue_dict["schema"],
-            consumer_class=queue_dict["consumer"],
-            producer_class=queue_dict["producer"],
+            schema=queue_dict["schema"],
+            consumer=queue_dict["consumer"],
+            producer=queue_dict["producer"],
         )
         queues[q_name] = meta
 
     return queues
+
+
+def queues_from_dict(config: dict[str, Any]) -> Dict[str, Queue]:
+    """loads config from user provided dictionary.
+
+    Overrides anything in yaml.
+
+    Returns a map of {queue_name: Queue}
+    """
+
+    # put all queues configs into a list
+    queue_list = [v for k, v in config.items()]
+    for_default_check = {"queues": queue_list}
+    config = apply_defaults(for_default_check)
+    input_output_queues: Dict[str, Queue] = {}
+
+    for qname, config in config.items():
+        try:
+            input_output_queues[qname] = Queue(
+                name=qname,
+                value=config["value"],
+                schema=config["schema"],
+                type=config["type"],
+                consumer=config["consumer"],
+                producer=config["producer"],
+            )
+        except KeyError:
+            logger.warning(f"Queue '{qname}' not found in configuraiton")
+    return input_output_queues

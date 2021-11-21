1057c5611d0e37adb632e7d77876004330a3e9ce
Adam Hendel <adam.hendel@shipt.com>
2021-11-20 22:25:14 -0600

    plugins for serializers

---
+++ tests/test_serializers/test_json.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from json import JSONDecodeError
+
+import pytest
+
+from volley.serializers.json_serializer import JsonSerialization
+
+
+def test_success() -> None:
+    serializer = JsonSerialization()
+
+    msg = {"hello": "world", "number": 42}
+
+    serialized = serializer.serialize(msg)
+    assert isinstance(serialized, bytes)
+
+    deserialized = serializer.deserialize(serialized)
+
+    assert deserialized == msg
+
+
+def test_fail() -> None:
+    serializer = JsonSerialization()
+
+    msg = {"time": datetime.now(), "number": 42}
+    with pytest.raises(TypeError):
+        serializer.serialize(msg, default=None)
+
+    bad_json = "abc : 123}"
+    with pytest.raises(JSONDecodeError):
+        serializer.deserialize(bad_json)

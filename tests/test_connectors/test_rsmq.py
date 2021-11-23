50b629612094ea82ee19815c7b54141ccec56ad9
Adam Hendel <adam.hendel@shipt.com>
2021-11-23 13:54:42 -0600

    completed cov for redis connector

--- tests/test_connectors/test_rsmq.py
+++ tests/test_connectors/test_rsmq.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch, MagicMock
+
 from volley.connectors import RSMQConsumer, RSMQProducer
 from volley.data_models import QueueMessage
 
@@ -14,3 +16,15 @@ def test_rsmq_consumer(mock_rsmq_consumer: RSMQConsumer) -> None:
 def test_rsmq_delete(mock_rsmq_consumer: RSMQConsumer) -> None:
 
     assert mock_rsmq_consumer.delete_message(queue_name="test", message_id="abc123")
+
+
+@patch("volley.connectors.rsmq.RedisSMQ")
+def test_return_none(mocked_rsmq: MagicMock) -> None:
+    mocked_rsmq.queue.receiveMessage.return_value.exceptions.return_value.execute = (
+        None
+    )
+    consumer = RSMQConsumer(
+        host="redis", queue_name="test"
+    )
+    msg = consumer.consume(queue_name="test")
+    assert msg is None

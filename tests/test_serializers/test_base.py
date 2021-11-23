c5eefba36a71413bdceb0b0459928ff9ae2d936e
Adam Hendel <adam.hendel@shipt.com>
2021-11-23 13:14:27 -0600

    add test for base serializer handler

--- tests/test_serializers/test_base.py
+++ tests/test_serializers/test_base.py
@@ -1,4 +1,13 @@
-from volley.serializers.base import NullSerializer
+from unittest.mock import MagicMock, patch
+
+from pytest import fixture, raises
+
+from volley.serializers.base import NullSerializer, handle_serializer
+
+
+@fixture
+def null_serializer() -> NullSerializer:
+    return NullSerializer()
 
 
 def test_null_serialzer() -> None:
@@ -12,3 +21,18 @@ def test_null_serialzer() -> None:
 
     serialized = ser.serialize(msg)
     assert msg is serialized
+
+
+@patch("volley.serializers.base.NullSerializer")
+def test_handle_serializer_fail(serializer: MagicMock) -> None:
+    serializer.serialize.side_effect = Exception()
+    serializer.deserialize.side_effect = Exception()
+
+    raw_msg = b"abc"
+    for operation in ["deserialize", "serialize"]:
+        msg, status = handle_serializer(serializer=serializer, operation=operation, message=raw_msg)
+        assert msg == raw_msg
+        assert status is False
+
+    with raises(NotImplementedError):
+        handle_serializer(serializer=serializer, operation="xtz", message=raw_msg)

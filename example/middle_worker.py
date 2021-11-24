8ff934e455b122d386fe5542a99b547dafecf8ad
Adam Hendel <adam.hendel@shipt.com>
2021-11-24 16:21:02 -0600

    fixing examples

--- example/middle_worker.py
+++ example/middle_worker.py
@@ -1,15 +1,38 @@
 from random import randint
 from typing import Any, List, Tuple
 
+from example.data_models import OutputMessage, PostgresMessage
 from volley.data_models import ComponentMessage
 from volley.engine import Engine
 from volley.logging import logger
 
-eng = Engine(
-    input_queue="redis_queue",
-    output_queues=["postgres_queue", "output-topic"],
-    yaml_config_path="./example/volley_config.yml",
-)
+queue_config = {
+    # define queue configurations in a dict
+    # overrwrites any existing yaml configs
+    "redis_queue": {
+        "value": "long_name_1",
+        "type": "rsmq",
+        "schema": "dict",
+        "producer": "example.plugins.my_plugin.MyPGProducer",
+        "consumer": "example.plugins.my_plugin.MyPGConsumer",
+    },
+    "postgres_queue": {
+        "value": "my_long_table_name",
+        "type": "postgres",
+        "schema": "example.data_models.PostgresMessage",
+        # disable serializer - sqlachemy implementation in example/plugin/my_plugin.py handles this
+        "serializer": "disabled",
+        "producer": "example.plugins.my_plugin.MyPGProducer",
+        "consumer": "example.plugins.my_plugin.MyPGConsumer",
+    },
+    "output-topic": {
+        "value": "localhost.kafka.output",
+        "type": "kafka",
+        "schema": "example.data_models.OutputMessage",
+    },
+}
+
+eng = Engine(input_queue="redis_queue", output_queues=["postgres_queue", "output-topic"], queue_config=queue_config)
 
 
 @eng.stream_app
@@ -22,10 +45,12 @@ def main(msg: Any) -> List[Tuple[str, ComponentMessage]]:
 
     max_plus_jiggle = max_val + randint(1, 20)
 
-    output_msg = ComponentMessage(request_id=req_id, max_plus=max_plus_jiggle)
+    output_msg = OutputMessage(request_id=req_id, max_plus=max_plus_jiggle)
+    pg_msg = PostgresMessage(request_id=req_id, max_plus=max_plus_jiggle)
 
-    logger.info(f"{req_id=}: {max_val=}")
-    return [("postgres_queue", output_msg), ("output-topic", output_msg)]
+    logger.info(output_msg.dict())
+    logger.info(pg_msg.dict())
+    return [("postgres_queue", pg_msg), ("output-topic", output_msg)]
 
 
 if __name__ == "__main__":

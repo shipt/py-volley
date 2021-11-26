241fcd6cf5133132a25986e30ef8ad70fac76464
Adam Hendel <adam.hendel@shipt.com>
2021-11-25 22:16:15 -0600

    pass config to kafka connector

--- volley/connectors/kafka.py
+++ volley/connectors/kafka.py
@@ -19,25 +19,31 @@ class KafkaConsumer(Consumer):
     def __post_init__(self) -> None:
         self.host = os.environ["KAFKA_BROKERS"]
 
+        # self.config provided from base Consumer class
         # consumer group assignment
-        # use env var first, then command line argument w/ env, then auto generate one
-        try:
-            consumer_group = os.environ["KAFKA_CONSUMER_GROUP"]
-        except KeyError:
-            # TODO: need a better way to do this
-            # keeping to prevent breaking change
-            logger.warning("KAFKA_CONSUMER_GROUP not specified in environment")
+        # try config, then env var, then command line argument w/ env
+        if "group.id" in self.config:
+            # we'll pass the config directly into Kafka constructor
+            pass
+        else:
             try:
-                component_name = sys.argv[1]
-                consumer_group = f"{APP_ENV}_{component_name}"
-            except Exception:
-                logger.exception("Kafka Consumer group not specified")
-                raise
+                self.config["group.id"] = os.environ["KAFKA_CONSUMER_GROUP"]
+            except KeyError:
+                # TODO: need a better way to do this
+                # keeping to prevent breaking change
+                logger.warning("KAFKA_CONSUMER_GROUP not specified in environment")
+                try:
+                    component_name = sys.argv[1]
+                    self.config["group.id"] = f"{APP_ENV}_{component_name}"
+                except Exception:
+                    logger.exception("Kafka Consumer group not specified")
+                    raise
 
-        logger.info(f"Kafka {consumer_group=}")
-        self.consumer_group: str = consumer_group
+        self.consumer_group = self.config["group.id"]
+        logger.info(f"Kafka Configuration: {self.config}")
         self.c = KConsumer(
-            consumer_group=consumer_group,
+            consumer_group=self.config["group.id"],
+            config_override=self.config,
             # TODO: develop commit strategy to minimize duplicates and guarantee no loss
             # config_override={"enable.auto.offset.store": False}
         )

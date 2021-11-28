dec365eaaca5fb6fefe87e98b09973c58ae0b028
Adam Hendel <adam.hendel@shipt.com>
2021-11-28 17:15:10 -0600

    add msgpack to integration test

--- volley/serializers/msgpack_serializer.py
+++ volley/serializers/msgpack_serializer.py
@@ -6,10 +6,10 @@ from volley.serializers.base import BaseSerialization
 
 
 class MsgPackSerialization(BaseSerialization):
-    def serialize(self, message: dict[Any, Any], **kwargs) -> bytes:
+    def serialize(self, message: dict[Any, Any], *args: Any, **kwargs: Any) -> bytes:
         serialized: bytes = msgpack.packb(message)
         return serialized
 
-    def deserialize(self, message: bytes, **kwargs) -> dict[str, Any]:
+    def deserialize(self, message: bytes, *args: Any, **kwargs: Any) -> dict[str, Any]:
         deserialized: dict[str, Any] = msgpack.unpackb(message)
         return deserialized
--- volley/serializers/orjson_serializer.py
+++ volley/serializers/orjson_serializer.py
@@ -6,10 +6,10 @@ from volley.serializers.base import BaseSerialization
 
 
 class OrJsonSerialization(BaseSerialization):
-    def serialize(self, message: dict[Any, Any]) -> bytes:
+    def serialize(self, message: dict[Any, Any], *args: Any, **kwargs: Any) -> bytes:
         serialized: bytes = orjson.dumps(message, option=orjson.OPT_NAIVE_UTC, default=str)
         return serialized
 
-    def deserialize(self, message: bytes) -> dict[str, Any]:
+    def deserialize(self, message: bytes, *args: Any, **kwargs: Any) -> dict[str, Any]:
         deserialized: dict[str, Any] = orjson.loads(message)
         return deserialized

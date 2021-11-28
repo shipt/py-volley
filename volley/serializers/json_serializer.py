ede3b2ed0538dc1ef660138d11c5ec2860df9f5e
Adam Hendel <adam.hendel@shipt.com>
2021-11-28 17:15:10 -0600

    add msgpack to integration test

--- volley/serializers/json_serializer.py
+++ volley/serializers/json_serializer.py
@@ -5,10 +5,10 @@ from volley.serializers.base import BaseSerialization
 
 
 class JsonSerialization(BaseSerialization):
-    def serialize(self, message: dict[Any, Any], default: type = str) -> bytes:
+    def serialize(self, message: dict[Any, Any], default: type = str, *args: Any, **kwargs: Any) -> bytes:
         serialized: bytes = json.dumps(message, default=default).encode("utf-8")
         return serialized
 
-    def deserialize(self, message: bytes) -> dict[str, Any]:
+    def deserialize(self, message: bytes, *args: Any, **kwargs: Any) -> dict[str, Any]:
         deserialized: dict[str, Any] = json.loads(message)
         return deserialized

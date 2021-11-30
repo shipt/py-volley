be6212cd0dc08afbb0988d29f9c7922b4111da69
Adam Hendel <adam.hendel@shipt.com>
2021-11-30 00:41:51 -0600

    test for rsmq configs

--- tests/test_connectors/test_rsmq.py
+++ tests/test_connectors/test_rsmq.py
@@ -1,6 +1,9 @@
 from unittest.mock import MagicMock, patch
 
+from pytest import MonkeyPatch, raises
+
 from volley.connectors import RSMQConsumer, RSMQProducer
+from volley.connectors.rsmq import RSMQConfigError
 from volley.data_models import QueueMessage
 
 
@@ -26,3 +29,43 @@ def test_return_none(mocked_rsmq: MagicMock) -> None:
     assert msg is None
 
     consumer.on_fail()
+
+
+@patch("volley.connectors.rsmq.RedisSMQ")
+def test_init_config(mocked_rsmq: MagicMock, monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setenv("REDIS_HOST", "env_redis")
+    config = {
+        "queue_name": "overwridden",
+        "host": "override_redis",
+        "options": {"decode_responses": False},
+        "random": "overridden",
+    }
+    consumer = RSMQConsumer(queue_name="test", config=config.copy())
+    producer = RSMQProducer(queue_name="test", config=config.copy())
+    for k, v in config.items():
+        # all overwrides must be present
+        assert consumer.config[k] == v
+        assert producer.config[k] == v
+
+    del config["host"]
+    config["options"] = {"someother": "configs"}
+    consumer = RSMQConsumer(queue_name="test", config=config.copy())
+    producer = RSMQProducer(queue_name="test", config=config.copy())
+    assert consumer.config["host"] == "env_redis"
+    assert producer.config["host"] == "env_redis"
+    # and this is an important default
+    assert consumer.config["options"]["decode_responses"] is False
+
+    for k, v in config.items():
+        # all overwrides must be present
+        # host from env var
+        assert consumer.config[k] == v
+        assert producer.config[k] == v
+
+    monkeypatch.delenv("REDIS_HOST")
+    with raises(RSMQConfigError):
+        # no host provided - should crash
+        RSMQConsumer(queue_name="test", config=config.copy())
+    with raises(RSMQConfigError):
+        # no host provided - should crash
+        RSMQProducer(queue_name="test", config=config.copy())

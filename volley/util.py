7e1f533e484318467f7ff5bf4b84c35c941ef3a1
Adam Hendel <adam.hendel@shipt.com>
2021-12-01 16:19:26 -0600

    pylint

--- volley/util.py
+++ volley/util.py
@@ -9,5 +9,5 @@ class GracefulKiller:
         signal.signal(signal.SIGINT, self.exit_gracefully)
         signal.signal(signal.SIGTERM, self.exit_gracefully)
 
-    def exit_gracefully(self, signum: int, frame: Any) -> None:
+    def exit_gracefully(self, signum: int, frame: Any) -> None:  # pylint: disable=W0613
         self.kill_now = True

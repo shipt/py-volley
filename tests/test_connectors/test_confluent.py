8038484891d3acdafcd22a20db394af6673f63af
jmeisele <jmeisele@yahoo.com>
2021-12-28 11:38:42 -0600

    :construction: linter

--- tests/test_connectors/test_confluent.py
+++ tests/test_connectors/test_confluent.py
@@ -2,13 +2,13 @@ from unittest.mock import MagicMock, patch
 from uuid import uuid4
 
 import pytest
-from pytest import MonkeyPatch, raises
+from pytest import MonkeyPatch
 
-from tests.conftest import KafkaMessage, mock_confluent_producer
+from tests.conftest import KafkaMessage
 from volley.connectors import ConfluentKafkaConsumer, ConfluentKafkaProducer
-from volley.connectors.base import Consumer, Producer
 from volley.connectors.confluent import handle_creds
 from volley.data_models import QueueMessage
+
 
 def test_confluent_produce(mock_confluent_producer: ConfluentKafkaProducer) -> None:
     assert mock_confluent_producer.produce(queue_name="test-topic", message=b"{'foo':'bar'}")
@@ -18,18 +18,19 @@ def test_handle_creds(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.delenv("KAFKA_BROKERS")
     with pytest.raises(KeyError):
         handle_creds(config={})
-    
-def test_handle_creds_config_dict(monkeypatch: MonkeyPatch):
+
+
+def test_handle_creds_config_dict(monkeypatch: MonkeyPatch) -> None:
     monkeypatch.setenv("KAFKA_KEY", "get")
     monkeypatch.setenv("KAFKA_SECRET", "them")
     result = handle_creds(config={})
-    assert result["sasl.username"] is "get"
-    assert result["sasl.password"] is "them"
-    assert result["security.protocol"] is "SASL_SSL"
-    assert result["sasl.mechanism"] is "PLAIN"
+    assert result["sasl.username"] == "get"
+    assert result["sasl.password"] == "them"
+    assert result["security.protocol"] == "SASL_SSL"
+    assert result["sasl.mechanism"] == "PLAIN"
 
 
-def test_confluent_consumer_no_consumer_group():
+def test_confluent_consumer_no_consumer_group() -> None:
     with pytest.raises(Exception):
         ConfluentKafkaConsumer(queue_name="input-topic")
 

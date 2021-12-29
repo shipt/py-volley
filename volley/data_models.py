30f23ce8230d7e1c7b8b5e44135350ada3d817d8
Adam Hendel <adam.hendel@shipt.com>
2021-12-29 10:51:28 -0600

    add license

--- volley/data_models.py
+++ volley/data_models.py
@@ -1,3 +1,7 @@
+# Copyright (c) Shipt.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from typing import TypeVar
 
 from volley.models.pydantic_model import ComponentMessage, QueueMessage

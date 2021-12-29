8d1e3716b55d84db76551df2cf7705801ceaa0e1
Adam Hendel <adam.hendel@shipt.com>
2021-12-29 10:51:28 -0600

    add license

--- volley/queues.py
+++ volley/queues.py
@@ -1,3 +1,7 @@
+# Copyright (c) Shipt.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from dataclasses import dataclass, field
 from enum import Enum, auto
 from typing import Any, Dict, Optional

d266afa8a4aad2666839f365fc7d91e4b42a6f2e
Adam Hendel <adam.hendel@shipt.com>
2022-01-22 18:29:56 -0600

    add int test

--- tests/integration_tests/test_integration.py
+++ tests/integration_tests/test_integration.py
@@ -1,9 +1,12 @@
 import json
+import os
+import queue
 import time
 from typing import Any, List
 from uuid import uuid4
 
 import pytest
+import redis
 from confluent_kafka import OFFSET_END, Consumer, Producer, TopicPartition
 
 from example.data_models import InputMessage
@@ -12,7 +15,9 @@ from volley.connectors import (
     AsyncConfluentKafkaConsumer,
     ConfluentKafkaConsumer,
     ConfluentKafkaProducer,
+    RSMQProducer,
 )
+from volley.connectors.rsmq import RSMQConsumer
 from volley.data_models import QueueMessage
 from volley.logging import logger
 
@@ -42,184 +47,175 @@ def consume_messages(consumer: Consumer, num_expected: int, serialize: bool = Tr
     return consumed_messages
 
 
-# @pytest.mark.integration
-# def test_end_to_end(int_test_producer: Producer, int_test_consumer: Consumer, environment: Environment) -> None:  # noqa
-#     """good data should make it all the way through app"""
-#     # get name of the input topic
-#     logger.info(f"{environment.input_topic=}")
+@pytest.mark.integration
+def test_end_to_end(int_test_producer: Producer, int_test_consumer: Consumer, environment: Environment) -> None:  # noqa
+    """good data should make it all the way through app"""
+    # get name of the input topic
+    logger.info(f"{environment.input_topic=}")
 
-#     # get some sample data
-#     data = InputMessage.schema()["examples"][0]
+    # get some sample data
+    data = InputMessage.schema()["examples"][0]
 
-#     # consumer the messages off the output topic
-#     consume_topic = environment.output_topic
-#     logger.info(f"{consume_topic=}")
+    # consumer the messages off the output topic
+    consume_topic = environment.output_topic
+    logger.info(f"{consume_topic=}")
 
-#     int_test_consumer.assign([TopicPartition(topic=consume_topic, partition=0, offset=OFFSET_END)])
-#     int_test_consumer.subscribe([consume_topic])
+    int_test_consumer.assign([TopicPartition(topic=consume_topic, partition=0, offset=OFFSET_END)])
+    int_test_consumer.subscribe([consume_topic])
 
-#     # create some unique request id for tracking
-#     test_messages = 3
-#     request_ids: List[str] = [f"test_{x}_{str(uuid4())[:5]}" for x in range(test_messages)]
-#     for req_id in request_ids:
-#         # publish the messages
-#         data["request_id"] = req_id
-#         int_test_producer.produce(environment.input_topic, value=json.dumps(data))
-#     int_test_producer.flush()
+    # create some unique request id for tracking
+    test_messages = 3
+    request_ids: List[str] = [f"test_{x}_{str(uuid4())[:5]}" for x in range(test_messages)]
+    for req_id in request_ids:
+        # publish the messages
+        data["request_id"] = req_id
+        int_test_producer.produce(environment.input_topic, value=json.dumps(data))
+    int_test_producer.flush()
 
-#     consumed_messages = consume_messages(consumer=int_test_consumer, num_expected=len(request_ids))
-#     conusumed_ids = []
-#     for m in consumed_messages:
-#         # assert all consumed IDs were from the list we produced
-#         _id = m["request_id"]
-#         assert _id in request_ids
-#         conusumed_ids.append(_id)
+    consumed_messages = consume_messages(consumer=int_test_consumer, num_expected=len(request_ids))
+    conusumed_ids = []
+    for m in consumed_messages:
+        # assert all consumed IDs were from the list we produced
+        _id = m["request_id"]
+        assert _id in request_ids
+        conusumed_ids.append(_id)
 
-#     for _id in request_ids:
-#         # assert all ids we produced were in the list we consumed
-#         assert _id in conusumed_ids
+    for _id in request_ids:
+        # assert all ids we produced were in the list we consumed
+        assert _id in conusumed_ids
 
-#     assert len(request_ids) == len(conusumed_ids)
-
-
-# @pytest.mark.integration
-# def test_dlq_schema_violation(
-#     int_test_producer: Producer, int_test_consumer: Consumer, environment: Environment
-# ) -> None:
-#     """publish bad data to input queue
-#     it should cause schema violation and end up on DLQ
-#     """
-#     logger.info(f"{environment.input_topic=}")
-#     data = {"bad": "data"}
-#     logger.info(f"{environment.dlq=}")
-#     int_test_consumer.assign([TopicPartition(topic=environment.dlq, partition=0, offset=OFFSET_END)])
-#     int_test_consumer.subscribe([environment.dlq])
-
-#     # publish data to input-topic that does not meet schema requirements
-#     test_messages = 3
-#     request_ids: List[str] = [f"test_{x}_{str(uuid4())[:5]}" for x in range(test_messages)]
-#     for req_id in request_ids:
-#         # publish the messages
-#         data["request_id"] = req_id
-#         int_test_producer.produce(environment.input_topic, value=json.dumps(data))
-#     int_test_producer.flush()
-
-#     consumed_messages = []
-#     consumed_messages = consume_messages(consumer=int_test_consumer, num_expected=len(request_ids))
-
-#     conusumed_ids = []
-#     for m in consumed_messages:
-#         # assert all consumed IDs were from the list we produced
-#         _id = m["request_id"]
-#         assert _id in request_ids
-#         conusumed_ids.append(_id)
-
-#     logger.info(f"{conusumed_ids=}")
-#     for _id in request_ids:
-#         # assert all ids we produced were in the list we consumed
-#         assert _id in conusumed_ids
-
-#     assert len(request_ids) == len(conusumed_ids)
+    assert len(request_ids) == len(conusumed_ids)
 
 
-# @pytest.mark.integration
-# def test_dlq_serialization_failure(
-#     int_test_producer: Producer, int_test_consumer: Consumer, environment: Environment
-# ) -> None:
-#     """publish malformed json to input queue
-#     expect serialization failure and successful publish to the DLQ
-#     """
-#     logger.info(f"{environment.input_topic=}")
-#     # message missing closing quote on the key
-#     data = """{"malformed:"json"}"""
+@pytest.mark.integration
+def test_dlq_schema_violation(
+    int_test_producer: Producer, int_test_consumer: Consumer, environment: Environment
+) -> None:
+    """publish bad data to input queue
+    it should cause schema violation and end up on DLQ
+    """
+    logger.info(f"{environment.input_topic=}")
+    data = {"bad": "data"}
+    logger.info(f"{environment.dlq=}")
+    int_test_consumer.assign([TopicPartition(topic=environment.dlq, partition=0, offset=OFFSET_END)])
+    int_test_consumer.subscribe([environment.dlq])
 
-#     logger.info(f"{environment.dlq=}")
-#     int_test_consumer.assign([TopicPartition(topic=environment.dlq, partition=0, offset=OFFSET_END)])
-#     int_test_consumer.subscribe([environment.dlq])
+    # publish data to input-topic that does not meet schema requirements
+    test_messages = 3
+    request_ids: List[str] = [f"test_{x}_{str(uuid4())[:5]}" for x in range(test_messages)]
+    for req_id in request_ids:
+        # publish the messages
+        data["request_id"] = req_id
+        int_test_producer.produce(environment.input_topic, value=json.dumps(data))
+    int_test_producer.flush()
 
-#     # publish data to input-topic that does not meet schema requirements
-#     test_messages = 3
-#     request_ids: List[str] = [f"test_{x}_{str(uuid4())[:5]}" for x in range(test_messages)]
-#     for req_id in request_ids:
-#         # publish the messages
-#         _d = data + req_id
-#         # data is just an extremely messy byte string
-#         int_test_producer.produce(environment.input_topic, value=_d.encode("utf-8"))
-#     int_test_producer.flush()
+    consumed_messages = []
+    consumed_messages = consume_messages(consumer=int_test_consumer, num_expected=len(request_ids))
 
-#     # dont try to serialize - we already know it will fail serialization
-#     consumed_messages = consume_messages(consumer=int_test_consumer, num_expected=len(request_ids), serialize=False)
+    conusumed_ids = []
+    for m in consumed_messages:
+        # assert all consumed IDs were from the list we produced
+        _id = m["request_id"]
+        assert _id in request_ids
+        conusumed_ids.append(_id)
 
-#     conusumed_ids = []
-#     for m in consumed_messages:
-#         # assert all consumed IDs were from the list we produced
-#         _id = str(m).split("}")[-1]
-#         conusumed_ids.append(_id)
+    logger.info(f"{conusumed_ids=}")
+    for _id in request_ids:
+        # assert all ids we produced were in the list we consumed
+        assert _id in conusumed_ids
 
-#     for _id in request_ids:
-#         # assert all ids we produced were in the list we consumed
-#         assert _id in conusumed_ids
-
-#     assert len(request_ids) == len(conusumed_ids)
+    assert len(request_ids) == len(conusumed_ids)
 
 
-# @pytest.mark.integration
-# def test_confluent_consume(
-#     broker_config: dict[str, str], environment: Environment, int_test_consumer: Consumer
-# ) -> None:
-#     """offsets must commit properly
-#     publish some messages. consume them. commit offsets.
-#     """
-#     # ensure the consumer group starts at the high offset
-#     int_test_consumer.assign([TopicPartition(topic=environment.test_topic, partition=0, offset=OFFSET_END)])
-#     consumer = ConfluentKafkaConsumer(queue_name=environment.test_topic, config=broker_config, poll_interval=30)
+@pytest.mark.integration
+def test_dlq_serialization_failure(
+    int_test_producer: Producer, int_test_consumer: Consumer, environment: Environment
+) -> None:
+    """publish malformed json to input queue
+    expect serialization failure and successful publish to the DLQ
+    """
+    logger.info(f"{environment.input_topic=}")
+    # message missing closing quote on the key
+    data = """{"malformed:"json"}"""
 
-#     # send messages to test topic
-#     producer = ConfluentKafkaProducer(
-#         queue_name=environment.test_topic, config={"bootstrap.servers": environment.brokers}, daemon=False
-#     )
-#     # confluent producer needs consumer's on_success and on_fail, these are set by volley
-#     def dummy_callback(context: Any) -> None:
-#         pass
-#     producer.set_on_fail(dummy_callback)
-#     producer.set_on_success(dummy_callback)
+    logger.info(f"{environment.dlq=}")
+    int_test_consumer.assign([TopicPartition(topic=environment.dlq, partition=0, offset=OFFSET_END)])
+    int_test_consumer.subscribe([environment.dlq])
 
-#     num_test_message = 3
-#     for i in range(num_test_message):
-#         producer.produce(
-#             queue_name=environment.test_topic, message=f"message_{i}".encode("utf-8"), message_context=None
-#         )
-#     producer.shutdown()
+    # publish data to input-topic that does not meet schema requirements
+    test_messages = 3
+    request_ids: List[str] = [f"test_{x}_{str(uuid4())[:5]}" for x in range(test_messages)]
+    for req_id in request_ids:
+        # publish the messages
+        _d = data + req_id
+        # data is just an extremely messy byte string
+        int_test_producer.produce(environment.input_topic, value=_d.encode("utf-8"))
+    int_test_producer.flush()
 
-#     # consume one message, record offset but do not commit it, leave consumer group
-#     message_0: QueueMessage = consumer.consume()  # type: ignore
-#     offset_0 = message_0.message_context.offset()
-#     consumer.shutdown()
+    # dont try to serialize - we already know it will fail serialization
+    consumed_messages = consume_messages(consumer=int_test_consumer, num_expected=len(request_ids), serialize=False)
 
-#     # recreate the consumer and subscribe
-#     consumer = ConfluentKafkaConsumer(queue_name=environment.test_topic, config=broker_config, poll_interval=30)
-#     # consume message again, must be same offset that we previously consumed
-#     message_0a: QueueMessage = consumer.consume()  # type: ignore
-#     assert message_0a.message_context.offset() == offset_0
-#     # commit the offset, leave the consumer group
-#     consumer.on_success(message_context=message_0a.message_context)
-#     consumer.shutdown()
+    conusumed_ids = []
+    for m in consumed_messages:
+        # assert all consumed IDs were from the list we produced
+        _id = str(m).split("}")[-1]
+        conusumed_ids.append(_id)
 
-#     # recreate the consumer
-#     consumer = ConfluentKafkaConsumer(queue_name=environment.test_topic, config=broker_config, poll_interval=30)
-#     # consume message again, must be the next offset
-#     message_1: QueueMessage = consumer.consume()  # type: ignore
-#     offset_1 = message_1.message_context.offset()
-#     assert offset_1 == offset_0 + 1
-#     # commit the offset, leave the consumer group
-#     consumer.on_success(message_context=message_1.message_context)
-#     consumer.shutdown()
+    for _id in request_ids:
+        # assert all ids we produced were in the list we consumed
+        assert _id in conusumed_ids
 
-#     # use Confluent consumer directly, validate offset is also the next offset
-#     int_test_consumer.subscribe([environment.test_topic])
-#     message_2 = int_test_consumer.poll(30)
-#     assert message_2.offset() == offset_1 + 1
-#     int_test_consumer.close()
+    assert len(request_ids) == len(conusumed_ids)
+
+
+@pytest.mark.integration
+def test_confluent_consume(
+    broker_config: dict[str, str], environment: Environment, int_test_consumer: Consumer
+) -> None:
+    """offsets must commit properly
+    publish some messages. consume them. commit offsets.
+    """
+    # ensure the consumer group starts at the high offset
+    int_test_consumer.assign([TopicPartition(topic=environment.test_topic, partition=0, offset=OFFSET_END)])
+    consumer = ConfluentKafkaConsumer(queue_name=environment.test_topic, config=broker_config, poll_interval=30)
+
+    # send messages to test topic
+    producer = Producer({"bootstrap.servers": environment.brokers})
+
+    num_test_message = 3
+    for i in range(num_test_message):
+        producer.produce(topic=environment.test_topic, value=f"message_{i}".encode("utf-8"))
+    producer.flush()
+
+    # consume one message, record offset but do not commit it, leave consumer group
+    message_0: QueueMessage = consumer.consume()  # type: ignore
+    offset_0 = message_0.message_context.offset()
+    consumer.shutdown()
+
+    # recreate the consumer and subscribe
+    consumer = ConfluentKafkaConsumer(queue_name=environment.test_topic, config=broker_config, poll_interval=30)
+    # consume message again, must be same offset that we previously consumed
+    message_0a: QueueMessage = consumer.consume()  # type: ignore
+    assert message_0a.message_context.offset() == offset_0
+    # commit the offset, leave the consumer group
+    consumer.on_success(message_context=message_0a.message_context)
+    consumer.shutdown()
+
+    # recreate the consumer
+    consumer = ConfluentKafkaConsumer(queue_name=environment.test_topic, config=broker_config, poll_interval=30)
+    # consume message again, must be the next offset
+    message_1: QueueMessage = consumer.consume()  # type: ignore
+    offset_1 = message_1.message_context.offset()
+    assert offset_1 == offset_0 + 1
+    # commit the offset, leave the consumer group
+    consumer.on_success(message_context=message_1.message_context)
+    consumer.shutdown()
+
+    # use Confluent consumer directly, validate offset is also the next offset
+    int_test_consumer.subscribe([environment.test_topic])
+    message_2 = int_test_consumer.poll(30)
+    assert message_2.offset() == offset_1 + 1
+    int_test_consumer.close()
 
 
 @pytest.mark.integration
@@ -265,7 +261,7 @@ def test_confluent_async_consume(
     # produce another, will trigger callback from first message
     # do not enable the background thread, we will test that later
     # just testing the in-line poll()
-    producer1.init_callbacks(on_fail=consumer1.on_fail, on_success=consumer1.on_success, thread=False)
+    producer1.init_callbacks(consumer=consumer1, thread=False)
     time.sleep(1)
     producer1.produce(
         queue_name=environment.test_topic, message="message".encode("utf-8"), message_context=message_0.message_context
@@ -297,7 +293,7 @@ def test_confluent_async_consume(
     # producer will call poll(), but there should be no pending reports
     assert consumer2.last_offset == {}
     # init the callbacks
-    producer2.init_callbacks(on_fail=consumer2.on_fail, on_success=consumer2.on_success)
+    producer2.init_callbacks(consumer=consumer2)
     # there is a delay, so wait. this will call poll and change local state
     time.sleep(5)
     assert consumer2.last_offset[0] == offset_1
@@ -311,3 +307,48 @@ def test_confluent_async_consume(
     offset_2 = message_2.message_context.offset()
     assert offset_1 == offset_2 - 1
     consumer3.shutdown()
+
+
+@pytest.mark.integration
+def test_redis_to_kafka(environment: Environment) -> None:
+    """consumes from redis, produce async to kafka, delete w/ callback"""
+    REDIS_HOST = os.environ["REDIS_HOST"]
+    INPUT = "test-input-redis"
+    r = redis.Redis(host=REDIS_HOST)
+    from rsmq import RedisSMQ
+
+    queue = RedisSMQ(host=REDIS_HOST, qname=INPUT)
+    queue.deleteQueue().exceptions(False).execute()
+
+    _producer = RSMQProducer(host=REDIS_HOST, queue_name=INPUT)
+    consumer = RSMQConsumer(host=REDIS_HOST, queue_name=INPUT)
+    producer = ConfluentKafkaProducer(
+        queue_name=environment.redis_to_kafka_topic,
+        config={"bootstrap.servers": environment.brokers},
+    )
+    NUM_TEST = 5
+    # put some test data in Redis queue
+    for m in range(NUM_TEST):
+        message = f"{m}_test_msg".encode("utf-8")
+        _producer.produce(queue_name=INPUT, message=message)
+
+    # assert size of the queue
+    assert r.zcard(f"rsmq:{INPUT}") == NUM_TEST
+
+    # init the callbacks
+    producer.init_callbacks(consumer=consumer)
+    # consume and produce the messages
+    consumed_ids = []
+    for _ in range(NUM_TEST):
+        consumed_msg = consumer.consume()
+        if consumed_msg is None:
+            raise AssertionError("No messages")
+        consumed_ids.append(consumed_msg.message_context)
+        producer.produce(
+            queue_name=environment.redis_to_kafka_topic,
+            message=consumed_msg.message,
+            message_context=consumed_msg.message_context,
+        )
+    time.sleep(4)
+    # all messages should have been deleted
+    assert r.zcard(f"rsmq:{INPUT}") == 0

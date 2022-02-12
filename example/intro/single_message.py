7189e520da1110b804a01669b97133bdb562f527
Adam Hendel <adam.hendel@shipt.com>
2022-02-12 10:03:54 -0600

    add example

--- example/intro/single_message.py
+++ example/intro/single_message.py
@@ -1,7 +1,19 @@
 import json
+import os
 
-from confluent_kafka import Producer
+from confluent_kafka import Message, Producer
 
-producer = Producer({"bootstrap.servers": "localhost:9092"})
-producer.produce(topic="my.kafka.topic.name", value=json.dumps({"my_values": [1, 2, 3]}))
+
+def acked(err: str, msg: Message) -> None:
+    if err is not None:
+        print(
+            f"Failed delivery to {msg.topic()}, message: {msg.value()}, error: {err}",
+        )
+    else:
+        print(f"Successful delivery to {msg.topic()}, partion: {msg.partition()}, offset: {msg.offset()}")
+
+
+producer = Producer({"bootstrap.servers": os.getenv("KAFKA_BROKERS", "localhost:9092")})
+producer.produce(topic="my.kafka.topic.name", value=json.dumps({"my_values": [1, 2, 3]}), callback=acked)
 producer.flush(10)
+print("Published single message")

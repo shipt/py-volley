83a02a217f1af3a8d5edbc1d19bcf5fcf1321245
Adam <hendel.adam@gmail.com>
2022-03-16 23:06:48 -0500

    zmq

--- volley/connectors/__init__.py
+++ volley/connectors/__init__.py
@@ -1,9 +1,12 @@
 from volley.connectors.confluent import ConfluentKafkaConsumer, ConfluentKafkaProducer
 from volley.connectors.rsmq import RSMQConsumer, RSMQProducer
+from volley.connectors.zmq import ZMQConsumer, ZMQProducer
 
 __all__ = [
     "ConfluentKafkaConsumer",
     "ConfluentKafkaProducer",
     "RSMQConsumer",
     "RSMQProducer",
+    "ZMQConsumer",
+    "ZMQProducer",
 ]

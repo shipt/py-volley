f2cadecbaf88b21ad6f78bdd95d2f48c511a99b2
Adam Hendel <ChuckHend@users.noreply.github.com>
2022-07-18 08:06:53 -0500

    typed queue configuration (#103)
    * fix types
    
    * update fixture
    
    * less complex to_dict
    
    * remove unsed typevar
    
    * docstring for QueueConfig
    
    * update toc in mkdoc
    
    * remove eos
    
    * line len
    
    * update example in readme
    
    * missing comma

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,9 @@
+from typing import Any, Dict
+
 import pytest
 from pydantic.main import BaseModel
 
-from volley.config import load_yaml
+from volley.config import QueueConfig, load_yaml
 from volley.data_models import QueueMessage
 from volley.queues import import_module_from_string
 
@@ -24,3 +26,22 @@ def test_import_module_from_string() -> None:
 
     assert issubclass(class_module, BaseModel)
     assert isinstance(instance, QueueMessage)
+
+
+def test_typed_config() -> None:
+    """initialize single queue config"""
+    cfg = QueueConfig(name="test-cfg", value="my-queue", profile="confluent", config={"random": "configValue"})
+    cfg_dict = cfg.to_dict()
+    for key in ["model_handler", "consumer", "producer", "model_handler", "data_model", "serializer"]:
+        assert key not in cfg_dict
+    assert "profile" in cfg_dict
+    assert "config" in cfg_dict
+
+
+def test_typed_config_init(config_dict: Dict[str, Dict[str, Any]]) -> None:
+    # : Dict[str, Dict[str, str]]
+    configs = {}
+    for _, v in config_dict.items():
+        _cfg = QueueConfig(**v).to_dict()
+        configs[_cfg["name"]] = _cfg
+    assert configs == config_dict

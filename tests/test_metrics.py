60582e171dfe5da092ed899374d43822a3dcd24a
Adam <hendel.adam@gmail.com>
2022-10-05 17:44:14 -0500

    mock http

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,12 +1,10 @@
 import time
 import urllib.request
+from unittest.mock import MagicMock, patch
 
 import pytest
-from prometheus_client import Counter
 
 from volley.metrics import serve_metrics
-
-c = Counter("test_counter", "random description")
 
 
 def test_multiproc_metric_server(monkeypatch: pytest.MonkeyPatch) -> None:
@@ -17,14 +15,18 @@ def test_multiproc_metric_server(monkeypatch: pytest.MonkeyPatch) -> None:
     time.sleep(0.5)
     resp = urllib.request.urlopen(f"http://0.0.0.0:{port}/metrics")
     assert resp.status == 200
-    assert "Multiprocess" in resp.read().decode("utf-8")
+
+    with patch("volley.metrics.multiproc_collector_server") as mock_http, patch(
+        "volley.metrics.uvicorn.run"
+    ) as mock_uvicorn:
+        serve_metrics(port=port)
+    mock_http.assert_called_once()
+    mock_uvicorn.assert_called_once()
 
 
 def test_metric_server(monkeypatch: pytest.MonkeyPatch) -> None:
     """test the single process collector"""
     port = 1235
-    serve_metrics(port=port)
-    time.sleep(0.5)
-    resp = urllib.request.urlopen(f"http://0.0.0.0:{port}/metrics")
-    assert resp.status == 200
-    assert "Multiprocess" not in resp.read().decode("utf-8")
+    with patch("volley.metrics.start_http_server") as mock_http:
+        serve_metrics(port=port)
+    mock_http.assert_called_once()

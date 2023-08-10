ceefa4905c33f3deb5b0a3141916f67e6dfd9470
Joshua Cardosi <97040989+joshcardosi@users.noreply.github.com>
2023-08-10 14:26:51 -0400

    First pass at upgrading pydantic

--- example/data_models.py
+++ example/data_models.py
@@ -1,70 +1,63 @@
 from typing import List, Optional
 
 from volley.data_models import GenericMessage
+from pydantic import ConfigDict
 
 
 class InputMessage(GenericMessage):
     request_id: str
     list_of_values: List[float]
     msg_counter: int = 0
-
-    class Config:
-        schema_extra = {
-            "examples": [
-                {
-                    "request_id": "request-id-1234",
-                    "list_of_values": [1, 2, 3, 4.5],
-                }
-            ]
-        }
+    model_config = ConfigDict(json_schema_extra={
+        "examples": [
+            {
+                "request_id": "request-id-1234",
+                "list_of_values": [1, 2, 3, 4.5],
+            }
+        ]
+    })
 
 
 class Queue1Message(GenericMessage):
     request_id: str
     max_value: float
     msg_counter: int = 0
-
-    class Config:
-        schema_extra = {
-            "examples": [
-                {
-                    "request_id": "request-id-1234",
-                    "max_value": 4.5,
-                }
-            ]
-        }
+    model_config = ConfigDict(json_schema_extra={
+        "examples": [
+            {
+                "request_id": "request-id-1234",
+                "max_value": 4.5,
+            }
+        ]
+    })
 
 
 class OutputMessage(GenericMessage):
     request_id: str
     max_plus: float
     msg_counter: int = 0
-
-    class Config:
-        schema_extra = {
-            "examples": [
-                {
-                    "request_id": "request-id-1234",
-                    "max_plus": 5.5,
-                }
-            ]
-        }
+    model_config = ConfigDict(json_schema_extra={
+        "examples": [
+            {
+                "request_id": "request-id-1234",
+                "max_plus": 5.5,
+            }
+        ]
+    })
 
 
 class PostgresMessage(GenericMessage):
     request_id: str
     max_plus: float
     msg_counter: Optional[int] = 0
-
-    class Config:
-        schema_extra = {
-            "examples": [
-                {
-                    "request_id": "request-id-1234",
-                    "max_plus": 5.5,
-                }
-            ]
-        }
+    model_config = ConfigDict(json_schema_extra={
+        "examples": [
+            {
+                "request_id": "request-id-1234",
+                "max_plus": 5.5,
+            }
+        ]
+    })
 
 
 class KafkaKafkaInput(GenericMessage):

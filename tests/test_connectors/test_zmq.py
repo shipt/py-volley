051a4d7085e05f1b83aec72adf518c7548d7344a
Damon Allison <damon@shipt.com>
2023-11-20 11:12:24 -0600

    Update comments

--- tests/test_connectors/test_zmq.py
+++ tests/test_connectors/test_zmq.py
@@ -23,20 +23,22 @@ def test_zmqconsumer(mocked_zsmq: MagicMock) -> None:
 @patch("volley.connectors.zmq._socket")
 @patch.dict(os.environ, {"ZMQ_PORT": "5555", "ZMQ_HOST": "test-host"}, clear=True)
 def test_zmqconsumer_env_host(mocked_zsmq: MagicMock) -> None:  # pylint: disable=W0613
-    """Verify ZMQ_HOST takes priority over ZMQ_PORT
+    """Verify ZMQ_HOST / ZMQ_PORT env vars are used when host / port is not
+    specified in config.
 
-    Setting "host" will allow us to set the host, but the host will never be
-    used (bug).
-
-    Verify that the internal config class accepts host / port env vars
-    appropriately.
-
+    *IMPORTANT*: The `host` is never used. It's always 0.0.0.0.
     """
 
     msg = "test-message"
     mocked_zsmq.recv.return_value = msg
-    consumer = ZMQConsumer(host="redis", queue_name="test")
+    # bug: The `host` attr is never used.
+    # verify config values take priority over env vars
+    consumer = ZMQConsumer(host="redis", queue_name="test", config={"host": "host", "port": "port"})
+    assert consumer.config["host"] == "host"
+    assert consumer.config["port"] == "port"
 
+    # fallback to env vars
+    consumer = ZMQConsumer(host="redis", queue_name="test")
     assert consumer.config["host"] == "test-host"
     assert consumer.config["port"] == "5555"
 
@@ -57,23 +59,25 @@ def test_zmqproducer(mocked_zsmq: MagicMock) -> None:  # pylint: disable=W0613
 @patch("volley.connectors.zmq._socket")
 @patch.dict(os.environ, {"ZMQ_PORT": "5555", "ZMQ_HOST": "test-host"}, clear=True)
 def test_zmqproducer_env_host(mocked_zsmq: MagicMock) -> None:  # pylint: disable=W0613
-    """Verify ZMQ_HOST takes priority over ZMQ_PORT
+    """Verify ZMQ_HOST / ZMQ_PORT env vars are used when host / port is not
+    specified in config.
 
-    Setting "host" will allow us to set the host, but the host will never be
-    used (bug).
-
-    Verify that the internal config class accepts host / port env vars
-    appropriately.
-
+    *IMPORTANT*: The `host` is never used. It's always 0.0.0.0.
     """
 
     msg = b"test-message"
 
     # The host set here *should* take priority over the env var, but it doesn't (bug).
     # The host is hardcoded to 0.0.0.0
-    producer = ZMQProducer(host="redis", queue_name="test")
+    # verify config values take priority over env vars
+    producer = ZMQProducer(host="redis", queue_name="test", config={"host": "host", "port": "port"})
+    assert producer.config["host"] == "host"
+    assert producer.config["port"] == "port"
 
+    # fallback to env vars
+    producer = ZMQProducer(host="redis", queue_name="test")
     assert producer.config["host"] == "test-host"
     assert producer.config["port"] == "5555"
+
     producer.produce(queue_name="test", message=msg)
     producer.shutdown()
